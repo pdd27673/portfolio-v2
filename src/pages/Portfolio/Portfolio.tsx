6e4f056aa28094fb6cce99a5f87fd244297d05be
pdd27673 <paul.doho.741@my.csun.edu>
2022-07-14 18:49:14 -0600

    added sass + basic page layout

---
+++ src/pages/Portfolio/Portfolio.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+type Props = {};
+
+const Portfolio = (props: Props) => {
+  return (
+    <div>
+      <div>Banner</div>
+      <div>Description</div>
+      <div>Biography</div>
+      <div>Education/Experience</div>
+      <div>Skills</div>
+      <div>Companies related</div>
+      <div>Latest projects</div>
+      <div>Testimonials</div>
+      <div>Contact me</div>
+      <div>Footer</div>
+    </div>
+  );
+};
+
+export default Portfolio;
