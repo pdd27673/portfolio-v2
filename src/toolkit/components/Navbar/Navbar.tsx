8e95af6a1eab4bdf1a78636f07304006995de7e6
pdd27673 <paul.doho.741@my.csun.edu>
2022-10-24 06:11:28 -0600

    added assests and header

--- src/toolkit/components/Navbar/Navbar.tsx
+++ src/toolkit/components/Navbar/Navbar.tsx
@@ -1,12 +1,25 @@
-import React from 'react'
+import React from "react";
+import { images } from "../../../constants";
+import "./Navbar.scss";
 
-import './Navbar.scss'
-type Props = {}
+type Props = {};
 
 const Navbar = (props: Props) => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav className="app__navbar">
+      <div className="app__navbar-logo nav-wrapper">
+        <img src={images.logo} alt="logo" />
+      </div>
+      <ul className="app__navbar-links">
+        {['home', 'about', 'work', 'skills', 'contact'].map((item) => (
+          <li className="app__flex p-text" key={`link-${item}`}>
+              <div />
+              <a href={`#${item}`}>{item}</a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
