dc363a32f6667acf81b65d977b40a57660833ea0
pdd27673 <paul.doho.741@my.csun.edu>
2022-10-24 05:20:26 -0600

    reworked structure

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { Navbar } from './toolkit/components';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <Navbar />
       <Header />
       <About />
